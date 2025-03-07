faf7df56f368047a86d81f51f65985bad1540ef8
DaviGGA <davigregorio.a@hotmail.com>
2025-03-07 17:12:48 -0300

    update message again

--- src/account/dtos/create-account.dto.ts
+++ src/account/dtos/create-account.dto.ts
@@ -4,6 +4,6 @@ export class CreateAccountDTO {
   @IsEmail({}, {message: "This provided email is not valid."})
   readonly email: string;
 
-  @IsStrongPassword({}, {message: "Password must be at least 8 characters long, contain at least one lowercase letter, one uppercase letter, one number, and one special symbol!" })
+  @IsStrongPassword({}, {message: "Password must be at least 8 characters long, contain at least one: lowercase and uppercase letter;number and special symbol." })
   readonly password: string;
 }
