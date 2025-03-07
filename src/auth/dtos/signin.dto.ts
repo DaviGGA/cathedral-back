2c2e528d492bbba7e2c07581bc930b146afa3e2f
DaviGGA <davigregorio.a@hotmail.com>
2025-03-07 17:07:27 -0300

    changed dto validation message

--- src/auth/dtos/signin.dto.ts
+++ src/auth/dtos/signin.dto.ts
@@ -1,9 +1,9 @@
 import { IsEmail, IsStrongPassword } from 'class-validator';
 
 export class SignInDTO {
-  @IsEmail()
+  @IsEmail({}, {message: "This provided email is not valid."})
   readonly email: string;
 
-  @IsStrongPassword()
+  @IsStrongPassword({}, {message: "Password must be at least 8 characters long, contain at least one lowercase letter, one uppercase letter, one number, and one special symbol!" })
   readonly password: string;
 }
